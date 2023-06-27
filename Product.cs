08b0e0d01bd85c708e44369a65f73140b7c0d8a6
dopecello <williamlocke.cello@gmail.com>
2023-06-27 12:43:02 -0400

    added constructors and methods using overloading

--- Product.cs
+++ Product.cs
@@ -13,39 +13,127 @@ namespace PieShop.InventoryMgmt
         private string? description;
 
         private int maxItemsInStock = 0;
+        
 
-        private UnitType unitType;
-        private int amountInStock = 0;
-        private bool isBelowStockThreshold = false;
+
+        public int Id
+        {
+            get { return id; } 
+            set
+            {
+                id = value;
+            }
+        }
+
+        public string Name
+        {
+            get { return name; }
+            set
+            {
+                name = value.Length > 50 ? value[..50] : value; // the [..50] dots indicate a range operator. It will register all chars up to 50.
+            }
+        }
+
+        public string? Description
+        {
+            get { return description; }
+            set
+            {
+                if (value == null)
+                {
+                    description = string.Empty;
+                }
+                else
+                {
+                    description = value.Length > 250 ? value[..250] : value;
+                }
+            }
+        }
+
+        public UnitType UnitType { get; set; }
+        public int AmountInStock { get; private set; } // this value is avail for use inside the class only.
+        public bool IsBelowStockThreshold { get; private set; }
+
+        public Product(int id) : this(id, string.Empty)
+        {
+        }
+
+        public Product(int id, string name)
+        {
+            Id = id;
+            Name = name;
+        }
+
+        public Product (int id, string name, string? description, UnitType unitType, int maxAmountInStock)
+        {
+            Id = id;
+            Name = name;
+            Description = description;
+            UnitType = unitType;
+
+            maxItemsInStock = maxAmountInStock;
+            
+            if (AmountInStock < 10)
+            {
+                IsBelowStockThreshold = true;
+            }
+        }
+
+        //public product(int id)
+        //{
+        //    id = id;
+        //} <=== having duplicate code is a bad practice
+
+        //public Product()
+        //{ } //default contructor: used to initialize an object
 
         public void UseProduct(int items)
         {
-            if (items <= amountInStock) 
+            if (items <= AmountInStock) 
             { //use the items
-              amountInStock -= items;
+              AmountInStock -= items;
                 UpdateLowStock();
-                Log($"Amount in stock updated. Now {amountInStock} items in stock.");
+                Log($"Amount in stock updated. Now {AmountInStock} items in stock.");
             }
             else
             {
-                Log($"Not enough items on stock for {CreateSimpleProductRepresentation()}. {amountInStock} available but {items} requested.");
+                Log($"Not enough items on stock for {CreateSimpleProductRepresentation()}. {AmountInStock} available but {items} requested.");
             }
         }
 
         public void IncreaseStock()
         {
-            amountInStock++;
+            AmountInStock++;
+        }
+
+        public void IncreaseStock(int amount) //this is a method overloading because this method, although the same, accepts one param, while the previous one doesn't accept any.
+        {
+            int newStock = AmountInStock + amount;
+
+            if (newStock <= maxItemsInStock)
+            {
+                AmountInStock += amount;
+            }
+            else
+            {
+                AmountInStock = maxItemsInStock; //only store the possible items, overstock isn't stored.
+                Log($"{CreateSimpleProductRepresentation} stock overflow. {newStock - AmountInStock} item(s) ordered that couldn't be stored.");
+            }
+            if (AmountInStock > 10)
+            {
+                IsBelowStockThreshold = false;
+            }
         }
 
         private void DecreaseStock(int items, string reason)
         {
-            if(items <= amountInStock)
+            if(items <= AmountInStock)
             {
-                amountInStock -= items;
+                AmountInStock -= items;
             }
             else
             {
-                amountInStock = 0;
+                AmountInStock = 0;
             }
 
             UpdateLowStock();
@@ -55,28 +143,31 @@ namespace PieShop.InventoryMgmt
 
         public string DisplayDetailsShort()
         {
-            return $"{id}. {name} \n{description}\n{amountInStock} items(s) in stock.";
+            return $"{id}. {name} \n{description}\n{AmountInStock} items(s) in stock.";
         }
 
         public string DisplayDetailsFull()
         {
-            StringBuilder sb = new();
-            //ToDo: add price here too
-            sb.Append($"{id} {name} \n{description}\n{amountInStock} item(s) in stock.");
+            return DisplayDetailsFull("");
+        }
 
-            if (isBelowStockThreshold)
+        public string DisplayDetailsFull(string extraDetails)
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.Append($"{Id} {Name} \n{Description}\n{AmountInStock} item(s) in stock.");
+            sb.Append(extraDetails);
+            if (IsBelowStockThreshold)
             {
                 sb.Append("\n!!STOCK LOW!!");
             }
-
             return sb.ToString();
         }
 
         public void UpdateLowStock()
         {
-            if(amountInStock < 10) //for now, a fixed value.
+            if(AmountInStock < 10) //for now, a fixed value.
             {
-                isBelowStockThreshold = true;
+                IsBelowStockThreshold = true;
             }
         }
 
